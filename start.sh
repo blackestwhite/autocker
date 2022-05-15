ddc9bfd2b051e858fb43437bfcdce18f627b0953
blackestwhite <pesaregoal@gmail.com>
2022-05-16 02:22:59 +0430

    fix via heredoc

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "#!/bin/bash
+cat > .git/hooks/post-merge <<- HDOC
+#!/bin/bash
 docker-compose down
-docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
-docker-compose up -d" >> ./.git/hooks/post-merge
+docker-compose up -d --build
+docker rm -v \$(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+docker rmi \$(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+HDOC
 chmod +x ./.git/hooks/post-merge
 chmod +x ./puller.sh
-./puller.sh &
